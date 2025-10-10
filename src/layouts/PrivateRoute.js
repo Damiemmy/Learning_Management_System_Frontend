ad02ee04cdb0b49b6ef98ac3137c612a0707f4d9
damiemmy <damisaemmanuel778@gmail.com>
2025-10-10 07:41:20 -0700

    Feature:Private Route,Mainwrapper

---
+++ src/layouts/PrivateRoute.js
@@ -0,0 +1,12 @@
+import { useEffect,useState } from "react";
+import {Navigate} from 'react-router-dom'
+import { useAuthStore } from "../store/auth";
+
+
+const PrivateRoute=({children})=>{
+    const loggedIn=useAuthStore((state)=>state.isLoggedin)();
+
+    return loggedIn?<>{children}</>:<Navigate to='/login'/>
+}
+
+export default PrivateRoute
